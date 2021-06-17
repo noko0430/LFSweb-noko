24778c5c0e02c3f0e2231507c35709114ac8379a
yamaoka shino <shino@yamaokanoMacBook-Air.local>
2021-06-17 19:43:35 +0900

    6.17更新

--- about.js
+++ about.js
@@ -2,4 +2,11 @@ $(function () {
     $('.nav_toggle').on('click',function(){
         $('.nav_toggle, .nav ul').toggleClass('open');
         });
+    var topBtn = $('.pagetop, .pagetop2');
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    }); 
     })
